49d310e18b0469fca0c5395ad59a648ce30a0396
salni84 <roberto.salerno@swissscom.com>
2020-12-19 17:13:58 +0100

    jenkinsfile

--- Poker-Frontend/e2e/src/app.e2e-spec.ts
+++ Poker-Frontend/e2e/src/app.e2e-spec.ts
@@ -13,6 +13,7 @@ describe('workspace-project App', () => {
     expect(page.getTitleText()).toEqual('Poker-Frontend app is running!');
   });
 
+  // @ts-ignore
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
