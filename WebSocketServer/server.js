d735c533855f59a6bf04e1592e02dfbc1a3c6b3f
Sebastian Rüegg <sebastian.rueegg@bluewin.ch>
2020-12-21 18:09:58 +0100

    Uses JSON
    - parses incoming data
    - sending message stringified

--- WebSocketServer/server.js
+++ WebSocketServer/server.js
@@ -6,16 +6,21 @@ const port = 6969;
 const server = http.createServer(express);
 const webSocketServer = new WebSocket.Server({ server })
 
-webSocketServer.on('connection', function connection(webSocketClient) {
-    webSocketClient.on('message', function incoming(data) {
-        webSocketServer.clients.forEach(function each(client) {
+webSocketServer.on('connection', (webSocketClient) => {
+    webSocketClient.on('message', (incomingMessage) => {
+        const message = incomingMessage ? JSON.parse(incomingMessage) : {};
+        webSocketServer.clients.forEach((client) => {
             if (client !== webSocketClient && client.readyState === WebSocket.OPEN) {
-                client.send(data);
+                client.send(JSON.stringify({
+                    type: message.type,
+                    text: message.text,
+                    user: message.user + ":",
+                }));
             }
         })
     })
 })
 
-server.listen(port, function() {
+server.listen(port, () => {
     console.log(`Server is listening on ${port}!`)
 })
