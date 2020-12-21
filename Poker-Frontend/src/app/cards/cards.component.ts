a801aa84ae09036ad2d01797bfa0605820fe7ca8
salni84 <roberto.salerno@swissscom.com>
2020-12-21 20:12:45 +0100

    selection-logic

--- Poker-Frontend/src/app/cards/cards.component.ts
+++ Poker-Frontend/src/app/cards/cards.component.ts
@@ -13,7 +13,7 @@ export class CardsComponent implements OnInit {
   toggle5: boolean = false;
   toggle8: boolean = false;
   toggle0: boolean = false;
-  hide: boolean = false;
+
 
 
   constructor() { }
@@ -21,26 +21,55 @@ export class CardsComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  setEstimation(id:number) {
+  selectCard(id:number) {
 
     if (id === 1) {
-        this.toggle1 = !this.toggle1;
-        this.hide = true
+      this.toggle1 = !this.toggle1;
+      this.toggle2 = false;
+      this.toggle3 = false;
+      this.toggle5 = false;
+      this.toggle8 = false;
+      this.toggle0 = false;
       }
     else if (id === 2) {
-      this.toggle2 = !this.toggle2
+      this.toggle2 = !this.toggle2;
+      this.toggle1 = false;
+      this.toggle3 = false;
+      this.toggle5 = false;
+      this.toggle8 = false;
+      this.toggle0 = false;
     }
     else if (id === 3) {
-      this.toggle3 = !this.toggle3
+      this.toggle3 = !this.toggle3;
+      this.toggle2 = false;
+      this.toggle1 = false;
+      this.toggle5 = false;
+      this.toggle8 = false;
+      this.toggle0 = false;
     }
     else if (id === 5) {
-      this.toggle5 = !this.toggle5
+      this.toggle5 = !this.toggle5;
+      this.toggle2 = false;
+      this.toggle3 = false;
+      this.toggle1 = false;
+      this.toggle8 = false;
+      this.toggle0 = false;
     }
     else if (id === 8) {
-      this.toggle8 = !this.toggle8
+      this.toggle8 = !this.toggle8;
+      this.toggle2 = false;
+      this.toggle3 = false;
+      this.toggle5 = false;
+      this.toggle1 = false;
+      this.toggle0 = false;
     }
     else if (id === 0) {
-      this.toggle0 = !this.toggle0
+      this.toggle0 = !this.toggle0;
+      this.toggle2 = false;
+      this.toggle3 = false;
+      this.toggle5 = false;
+      this.toggle8 = false;
+      this.toggle1 = false;
     }
   }
 }
