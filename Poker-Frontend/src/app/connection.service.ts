dfd4e97266fdc64dcd2e1422dcd952e3932d6e55
salni84 <roberto.salerno@swissscom.com>
2020-12-27 13:01:21 +0100

    deploy

--- Poker-Frontend/src/app/connection.service.ts
+++ Poker-Frontend/src/app/connection.service.ts
@@ -9,6 +9,6 @@ export class ConnectionService {
 
   constructor() { }
 
-  connection: WebSocketSubject<any> = webSocket({url: 'ws://localhost:6969', deserializer: e => e.data})
+  connection: WebSocketSubject<any> = webSocket({url: 'ws://semesterarbeit.herokuapp.com', deserializer: e => e.data})
 
 }
